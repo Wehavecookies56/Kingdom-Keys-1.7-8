48b9bd351033f840f4b94dd561180bbcd63186c6
Abelatox <abelatox@hotmail.com>
2014-09-14 13:05:43 +0200

    Nerfed the blocking XD

--- java/wehavecookies56/kk/core/event/HurtEvent.java
+++ java/wehavecookies56/kk/core/event/HurtEvent.java
@@ -5,7 +5,10 @@ import wehavecookies56.kk.item.AddedItems;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 
@@ -14,22 +17,16 @@ public class HurtEvent
 	@SubscribeEvent
 	public void onHurt(LivingHurtEvent event)
 	{
-
-		
-			if(event.entityLiving instanceof EntityPlayer)
-			 {
-				
-				EntityPlayer player = (EntityPlayer) event.entityLiving;
-				if(player.getHeldItem().getItem() != null && player.getHeldItem().getItem() == AddedItems.FrozenPride)
-					if(player.isBlocking())
-					{
-						{
-							player.setPotionEffect(Potion.moveSlowdown.id, 1, 1, 1.0F);
-							player.capabilities.setPlayerWalkSpeed(0);
-							event.ammount = 0;
-						}
-
-					}
-			 }
+		if(event.entityLiving instanceof EntityPlayer)
+		{
+			EntityPlayer player = (EntityPlayer) event.entityLiving;
+			if(player.getHeldItem().getItem() != null && player.getHeldItem().getItem() == AddedItems.FrozenPride)
+			{
+				if(player.isBlocking())
+				{	
+					event.ammount = 0;
+				}
+			}
+		}
 	}	
 }
--- java/wehavecookies56/kk/item/Org13/ItemFrozenPride.java
+++ java/wehavecookies56/kk/item/Org13/ItemFrozenPride.java
@@ -1,17 +1,24 @@
 package wehavecookies56.kk.item.org13;
 
+import ibxm.Player;
+
 import java.util.List;
 
 import mods.battlegear2.api.PlayerEventChild.OffhandAttackEvent;
 import mods.battlegear2.api.weapons.IBattlegearWeapon;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import wehavecookies56.kk.KingdomKeys;
+import wehavecookies56.kk.item.AddedItems;
 import wehavecookies56.kk.lib.ConfigBooleans;
 import wehavecookies56.kk.lib.Reference;
 import cpw.mods.fml.relauncher.Side;
@@ -38,7 +45,27 @@ public class ItemFrozenPride extends ItemSword implements IBattlegearWeapon{
     	}
     	return false;
     }
+    
+	@Override
+	public void onUpdate(ItemStack item, World world, Entity entity, int p_77663_4_, boolean p_77663_5_) 
+	{
+		EntityPlayer player = (EntityPlayer) entity;
 
+		if(player.getHeldItem().getItem() != null && player.getHeldItem().getItem() == AddedItems.FrozenPride)
+		{
+			if(player.isBlocking())
+			{	
+				player.capabilities.setPlayerWalkSpeed(0);
+				//player.addPotionEffect(new PotionEffect(Potion.moveSlowdown.id, 1, 6));
+			}
+			else
+			{
+				player.capabilities.setPlayerWalkSpeed(0.1F);
+			}
+		}
+    	super.onUpdate(item, world, entity, p_77663_4_, p_77663_5_);
+    }
+	
 	public ToolMaterial getMaterial() {
 		return this.material;
 	}
