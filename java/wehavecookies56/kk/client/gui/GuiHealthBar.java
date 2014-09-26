067629a11017b413ab9a92e3b54c4ca7d39b8df7
Abelatox <abelatox@hotmail.com>
2014-09-26 22:09:00 +0200

    Tabulated the GuihealthBar file and deleted a line
    in item hp

--- java/wehavecookies56/kk/client/gui/GuiHealthBar.java
+++ java/wehavecookies56/kk/client/gui/GuiHealthBar.java
@@ -18,24 +18,22 @@ public class GuiHealthBar extends Gui{
 	@SubscribeEvent
 	public void onRenderOverlayPost(RenderGameOverlayEvent event){
 		
-		if(event.type == RenderGameOverlayEvent.ElementType.TEXT){
-		
-		int guiWidth = 165;
-		int guiHeight = 9;
-		
-		Minecraft mc = Minecraft.getMinecraft();		
-		mc.renderEngine.bindTexture(new ResourceLocation("kk", "textures/gui/HPBar.png"));
-
-		int screenWidth = event.resolution.getScaledWidth();
-		int screenHeight = event.resolution.getScaledHeight();
-		EntityPlayer player = mc.thePlayer;
-		float oneHeart = (guiWidth / player.getMaxHealth());
-		int currHealth = guiWidth - (int) (oneHeart * player.getHealth());
-		GL11.glPushMatrix();
-		this.drawTexturedModalRect(screenWidth - guiWidth, screenHeight - guiHeight, 0, 0, guiWidth, guiHeight);
-		this.drawTexturedModalRect((screenWidth - guiWidth) + currHealth, screenHeight - guiHeight, 0, 8, (guiWidth - currHealth) - 2, guiHeight);
-
-		GL11.glPopMatrix();
+		if(event.type == RenderGameOverlayEvent.ElementType.TEXT)
+		{
+			int guiWidth = 165;
+			int guiHeight = 9;
+			int screenWidth = event.resolution.getScaledWidth();
+			int screenHeight = event.resolution.getScaledHeight();
+			Minecraft mc = Minecraft.getMinecraft();
+			EntityPlayer player = mc.thePlayer;
+			
+			mc.renderEngine.bindTexture(new ResourceLocation("kk", "textures/gui/HPBar.png"));
+			float oneHeart = (guiWidth / player.getMaxHealth());
+			int currHealth = guiWidth - (int) (oneHeart * player.getHealth());
+			GL11.glPushMatrix();
+			this.drawTexturedModalRect(screenWidth - guiWidth, screenHeight - guiHeight, 0, 0, guiWidth, guiHeight);
+			this.drawTexturedModalRect((screenWidth - guiWidth) + currHealth, screenHeight - guiHeight, 0, 8, (guiWidth - currHealth) - 2, guiHeight);
+			GL11.glPopMatrix();
 		}
 	}
 }
