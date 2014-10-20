2e359b90c3283ce76e40e20aaad7136716c5f2e7
Toby Wishart <qwenit56@gmail.com>
2014-10-20 20:21:48 +0100

    Rendering armour

--- java/wehavecookies56/kk/KingdomKeys.java
+++ java/wehavecookies56/kk/KingdomKeys.java
@@ -15,6 +15,7 @@ import org.apache.logging.log4j.Logger;
 import wehavecookies56.kk.achievements.AddedAchievments;
 import wehavecookies56.kk.api.synthesis.RecipeHandler;
 import wehavecookies56.kk.block.AddedBlocks;
+import wehavecookies56.kk.client.render.PlayerRender;
 import wehavecookies56.kk.core.event.DarkHeartDrops;
 import wehavecookies56.kk.core.event.DriveOrbDrops;
 import wehavecookies56.kk.core.event.EntityConstructEvent;
@@ -69,6 +70,7 @@ import wehavecookies56.kk.updater.Update;
 import wehavecookies56.kk.worldgen.ComponentRecipeHome;
 import wehavecookies56.kk.worldgen.VillagerCreationHandler;
 import wehavecookies56.kk.worldgen.WorldGenBlox;
+import api.player.render.RenderPlayerAPI;
 import cpw.mods.fml.client.event.ConfigChangedEvent;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.FMLLog;
@@ -142,7 +144,7 @@ public class KingdomKeys {
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		//ModelPlayerAPI.register("kk", PlayerModel.class);
-		//RenderPlayerAPI.register("kk", PlayerRender.class);
+		RenderPlayerAPI.register("kk", PlayerRender.class);
 		AddedItems.initKeyBlades();
 		AddedItems.initHearts();
 		AddedItems.initOthers();
--- java/wehavecookies56/kk/client/render/PlayerRender.java
+++ java/wehavecookies56/kk/client/render/PlayerRender.java
@@ -1,11 +1,14 @@
-/*package wehavecookies56.kk.client.render;
+package wehavecookies56.kk.client.render;
 
 import org.lwjgl.opengl.GL11;
 
-import wehavecookies56.kk.client.model.PlayerModel;
-
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
 import api.player.render.RenderPlayerAPI;
 import api.player.render.RenderPlayerBase;
 
@@ -17,14 +20,66 @@ public class PlayerRender extends RenderPlayerBase {
 		
 	}
 	
+	@Override
+	public void renderModel(EntityLivingBase paramEntityLivingBase, float paramFloat1, float paramFloat2, float paramFloat3, float paramFloat4, float paramFloat5, float paramFloat6) {
+		Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("kk", "textures/armor/KEYBLADEA_1.png"));
+		ModelBiped armour = renderPlayer.modelArmor;
+		armour.onGround = 0.0F;
+		armour.setRotationAngles(paramFloat1, paramFloat2, paramFloat3, paramFloat4, paramFloat5, paramFloat6, paramEntityLivingBase);
+		armour.bipedHead.showModel = true;
+		armour.bipedHead.render(0.0625F);
+		
+		armour.bipedHeadwear.showModel = true;
+		armour.bipedHeadwear.render(0.0625F);
+		
+		armour.bipedBody.showModel = true;
+		armour.bipedBody.render(0.0625F);
+		
+		armour.bipedLeftLeg.showModel = true;
+		armour.bipedLeftLeg.render(0.0625F);
+		
+		armour.bipedRightLeg.showModel = true;
+		armour.bipedRightLeg.render(0.0625F);
+				
+		armour.bipedLeftArm.showModel = true;
+		armour.bipedLeftArm.render(0.0625F);
+		
+		armour.bipedRightArm.showModel = true;
+		armour.bipedRightArm.render(0.0625F);
+		
+		Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("kk", "textures/armor/KEYBLADEA_2.png"));
+		
+		armour.bipedLeftLeg.showModel = true;
+		armour.bipedLeftLeg.render(0.0625F);
+		
+		armour.bipedRightLeg.showModel = true;
+		armour.bipedRightLeg.render(0.0625F);
+		
+		super.renderModel(paramEntityLivingBase, paramFloat1, paramFloat2, paramFloat3, paramFloat4, paramFloat5, paramFloat6);
+		
+	}
+	
+	/*@Override
+	public void beforeRenderPlayer(AbstractClientPlayer paramAbstractClientPlayer, double paramDouble1, double paramDouble2, double paramDouble3, float paramFloat1, float paramFloat2) {
+		Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("kk", "textures/armor/KEYBLADEA_1.png"));
+		ModelBiped armour = renderPlayer.modelArmor;
+		armour.onGround = 0.0F;
+		armour.setRotationAngles(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0625F, paramAbstractClientPlayer);
+		armour.bipedHead.showModel = true;
+		armour.bipedHead.rotateAngleZ = (float)Math.PI;
+		armour.bipedHead.render(0.0625F);
+		
+		super.afterRenderPlayer(paramAbstractClientPlayer, paramDouble1, paramDouble2, paramDouble3, paramFloat1, paramFloat2);
+	}*/
 	
 	@Override
 	public void renderFirstPersonArm(EntityPlayer paramEntityPlayer) {
+		Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("kk", "textures/armor/KEYBLADEA_1.png"));
+		ModelBiped armour = renderPlayer.modelArmor;
 		
-		renderPlayer.modelBipedMain.bipedRightArm.rotateAngleX = 2;
-		
+		armour.bipedRightArm.showModel = true;
+		armour.bipedRightArm.render(0.0625F);
 		super.renderFirstPersonArm(paramEntityPlayer);
 	}
 
 }
-*/
