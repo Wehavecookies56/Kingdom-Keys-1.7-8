508c2760d274c7434bf8c5775033bf7183b437e3
Toby Wishart <qwenit56@gmail.com>
2015-02-01 20:28:15 +0000

    Version num

--- java/wehavecookies56/kk/lib/Reference.java
+++ java/wehavecookies56/kk/lib/Reference.java
@@ -3,7 +3,7 @@ package wehavecookies56.kk.lib;
 public class Reference {
 	public static final String MOD_ID = "kk";
 	public static final String MOD_NAME = "KingdomKeys";
-	public static final String MOD_VER = "0.7.0.1.7.10-PRE";
+	public static final String MOD_VER = "0.7.0.1.7.10";
 	public static final String MOD_DEP = "required-after:Forge";
 	public static final String MOD_CHANNEL = "whckk";
 }
