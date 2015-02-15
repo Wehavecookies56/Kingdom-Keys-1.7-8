c6a22471cc96413eb70570d0c17212d6f860426e
Toby Wishart <qwenit56@gmail.com>
2015-02-15 12:32:30 +0000

    Removing bg2 api things we don't need

--- java/wehavecookies56/kk/item/ItemDarkHeart.java
+++ java/wehavecookies56/kk/item/ItemDarkHeart.java
@@ -1,8 +1,5 @@
 package wehavecookies56.kk.item;
 
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import wehavecookies56.kk.lib.Reference;
 import wehavecookies56.kk.lib.Strings;
 
 public class ItemDarkHeart extends ItemKingdomKeys{
@@ -12,9 +9,4 @@ public class ItemDarkHeart extends ItemKingdomKeys{
         
     }
     
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void registerIcons(IIconRegister par1IconRegister) {
-        itemIcon = par1IconRegister.registerIcon(Reference.MOD_ID + ":misc/" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".")+1));
-        }
 }
--- java/wehavecookies56/kk/item/ItemDarkLeather.java
+++ java/wehavecookies56/kk/item/ItemDarkLeather.java
@@ -1,8 +1,7 @@
 package wehavecookies56.kk.item;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import wehavecookies56.kk.lib.Reference;
 import wehavecookies56.kk.lib.Strings;
 
@@ -12,9 +11,4 @@ public class ItemDarkLeather extends ItemKingdomKeys{
         super();       
         this.setUnlocalizedName(Strings.DLeather);  
     }
-    @SideOnly(Side.CLIENT)
-	@Override
-    public void registerIcons(IIconRegister par1IconRegister) {
-        itemIcon = par1IconRegister.registerIcon(Reference.MOD_ID + ":misc/" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".")+1));
-        }
 }
